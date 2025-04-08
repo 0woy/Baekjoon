4d75b18ba04faa8a3a69b01f835e2f3f96f80509
0woy <qkrdbsdk1030@naver.com>
2025-04-08 22:08:34 +0900

    알파벳 찾기

--- src/Array/N_10809.java
+++ src/Array/N_10809.java
@@ -1,6 +1,10 @@
+package Array;
+
 import java.io.*;
+import java.util.Arrays;
 
 public class N_10809 {
+    /** Previous Solve
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
@@ -14,4 +18,24 @@ public class N_10809 {
         bw.flush();
         bw.close();
     }
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String str = br.readLine();
+        int [] alpha = new int[26];
+        Arrays.fill(alpha, -1);
+        for(int i=0;i<str.length();i++){
+            char cur = str.charAt(i);
+            if(alpha[cur-'a']==-1){
+                alpha[cur-'a']=i;
+            }
+        }
+        for(int v: alpha){
+            bw.write(v+" ");
+        }
+        bw.flush();
+        bw.close();
+    }
+
 }
